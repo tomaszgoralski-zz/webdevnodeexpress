dc1ce205f7f3bd8e02fabdc347b834fbd7764d3b
Tomasz Goralski <goralski.tomasz@qiagen.com>
2015-06-29 12:51:43 +0200

    chapter 3

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
 var express = require('express');
 var app = express();
-app.set('port', process.env.PORT || 3000);
-
 var handlebars = require('express3-handlebars')
     .create({
         defaultLayout: 'main'
     });
 app.engine('handlebars', handlebars.engine);
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'handlebars');
-
-//app.set('view engine', 'jade');
-//app.configure('development', function () { app.locals.pretty = true; });
-
-
-//app.get('/index', function (req, res) {
-//    res.type('text/html');
-//    res.render('index', {
-//        header: 'header go'
-//    });
-//});
-
-app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.render('home');
 });
-app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+app.get('/about', function(req, res) {
+    var fortunes = [
+        "Conquer your fears or they will conquer you.",
+        "Rivers need springs.",
+        "Do not fear what you don't know.",
+        "You will have a pleasant surprise.",
+        "Whenever possible, keep it simple.",
+    ];
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about',{
+        fortune : randomFortune
+    });
 });
-// custom 404 page
-app.use(function (req, res, next) {
-    res.type('text/plain');
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
-// custom 500 page
-app.use(function (err, req, res, next) {
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
+
+
+
+
+//app.get('/', function (req, res) {
+//    res.type('text/plain');
+//    res.send('Meadowlark Travel');
+//});
+//app.get('/about', function (req, res) {
+//    res.type('text/plain');
+//    res.send('About Meadowlark Travel');
+//});
+//// custom 404 page
+//app.use(function (req, res, next) {
+//    res.type('text/plain');
+//    res.status(404);
+//    res.send('404 - Not Found');
+//});
+//// custom 500 page
+//app.use(function (err, req, res, next) {
+//    console.error(err.stack);
+//    res.type('text/plain');
+//    res.status(500);
+//    res.send('500 - Server Error');
+//});
 app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate.');
